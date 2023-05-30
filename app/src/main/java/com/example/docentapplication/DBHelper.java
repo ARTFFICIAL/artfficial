b08e7c1a211dbd726ce699e26034fc6a17f2fdfb
jy375876 <jy375876@naver.com>
2023-05-30 11:56:18 +0900

    230530

---
+++ app/src/main/java/com/example/docentapplication/DBHelper.java
@@ -0,0 +1,156 @@
+package com.example.docentapplication;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "myDB.db";
+    public static final String Table_Name = "mytable";
+    public static final int version = 1;
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_CONTENTS = "contents";
+    private Context context;
+
+    public DBHelper(Context context) {
+        super(context, DB_NAME, null, version);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE if not exists "
+                + Table_Name + " (" + COLUMN_TITLE + " TEXT primary key, "
+                + COLUMN_CONTENTS + " TEXT);";
+
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sql = "DROP TABLE if exists " + Table_Name;
+        db.execSQL(sql);
+        onCreate(db);
+    }
+
+    // mytable 데이터 입력
+    public void insert(String name, String contents) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_TITLE, name);
+        cv.put(COLUMN_CONTENTS, contents);
+
+        db.insert(Table_Name, null, cv);
+        //db.execSQL("INSERT INTO " + Table_Name + " VALUES ('" + name + "', '" + contents + "');");
+        /*if (result == -1)
+        {
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(context, "데이터 추가 성공", Toast.LENGTH_SHORT).show();
+        }*/
+        db.close();
+    }
+
+    // mytable 데이터 삭제
+    public void Delete(String name) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("DELETE " + Table_Name + " WHERE " + COLUMN_TITLE + " = '" + name + "'");
+        db.close();
+    }
+
+    public void SelectAll (){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor cs = db.query(Table_Name, null,null,null,null,
+                null,null);
+        while(cs.moveToNext()){
+            System.out.println(cs.getString(0)+ " " + cs.getString(1));
+        }
+        //ContentValues cv = new ContentValues();
+        //db.execSQL("SELECT * FROM "+ Table_Name );
+        db.close();
+
+//        cv.get(COLUMN_TITLE, name);
+//        cv.get(COLUMN_CONTENTS, content);
+
+
+    }
+    /*
+       // 데이터 불러오기
+       public ArrayList getDB() {
+           ArrayList mk_info = new ArrayList<String>();
+
+           String sql = "SELECT * FROM " + Table_Name;
+
+           Cursor cursor = getWritableDatabase().rawQuery(sql,null);
+           if(cursor!=null && cursor.moveToFirst()){
+               do {
+                   mk_info.add(cursor.getString(1));
+               }while(cursor.moveToNext());
+           }
+
+           cursor.close();
+
+           return mk_info;
+
+           //DB open
+           SQLiteDatabase db = getReadableDatabase();
+
+           Cursor cursor = db.rawQuery("Select * from " + Table_Name, null);
+
+           while(cursor.moveToNext()) {
+               mk_info.add(cursor.getString(0)
+                       +"\n"
+                       +mk_info.add(cursor.getString(1)));
+           }
+           return mk_info;
+
+
+       }
+
+    public static List<ListViewAdapterData> selectAllmark() {
+        List<ListViewAdapterData> mkList = new ArrayList<ListViewAdapterData>();
+
+        String sql = "SELECT * FROM " + Table_Name +";";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor cursor = db.rawQuery(sql, null);
+
+        if(cursor.moveToFirst()) {
+            do {
+                mkList.add(new ListViewAdapterData(cursor.getString(0), cursor.getString(1)));
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+
+        return mkList;
+    } */
+
+//    public void getDB() {
+//        ArrayList mk_info = new ArrayList<String>();
+//
+//        String sql = "SELECT * FROM " + Table_Name;
+//
+//        Cursor cursor = getWritableDatabase().rawQuery(sql, null);
+//        if (cursor != null && cursor.moveToFirst()) {
+//            do {
+//                mk_info.add(cursor.getString(1));
+//            } while (cursor.moveToNext());
+//        }
+//
+//        cursor.close();
+//    }
+}
---
+++ app/src/main/java/com/example/docentapplication/MessageAdapter.java
@@ -0,0 +1,135 @@
+package com.example.docentapplication;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import static com.google.android.material.internal.ContextUtils.getActivity;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MyViewHolder> {
+
+    List<Message> messageList;
+
+    DBHelper helper;
+    SQLiteDatabase db;
+
+    public MessageAdapter(List<Message> messageList) {
+        this.messageList = messageList;
+//        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View chatView = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_item, null);
+        MyViewHolder myViewHolder = new MyViewHolder(chatView);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        Message message = messageList.get(position);
+
+        holder.starBtn.setTag(position);
+        holder.leftChatTextView.setTag(position);
+        holder.rightChatTextView.setTag(position);
+
+        if(message.getSentBy().equals(Message.SENT_BY_ME)){
+            holder.leftChatView.setVisibility(View.GONE);
+            holder.rightChatView.setVisibility(View.VISIBLE);
+            holder.rightChatTextView.setText(message.getMessage());
+        }else{
+            holder.rightChatView.setVisibility(View.GONE);
+            holder.leftChatView.setVisibility(View.VISIBLE);
+            holder.leftChatTextView.setText(message.getMessage());
+            holder.starBtn.setChecked(message.getChecked());
+        }
+
+        holder.starBtn.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+                if(holder.starBtn.isChecked()){
+                    message.setChecked(true);
+                    String index = holder.leftChatTextView.getTag().toString();
+                    int idx = Integer.parseInt(index);
+
+                    Message msg = (Message) messageList.get(idx-1);
+                    String question = msg.getMessage();
+
+                    System.out.println("질문 확인하기 : " + question);
+                    System.out.println("답 인덱스 확인하기 : " + holder.leftChatTextView.getTag());
+                    System.out.println("답 확인하기 : " + holder.leftChatTextView.getText());
+
+                    String answer =  holder.leftChatTextView.getText().toString().trim();
+
+                    if(question.contains("'") || answer.contains("'")){
+                        question = question.replace("'", "''");
+                    }
+                    if(answer.contains("'")){
+                        answer = answer.replace("'", "''");
+                    }
+
+                    DBHelper helper = new DBHelper(holder.itemView.getContext());//, DBHelper.DB_NAME, null, 1
+                    SQLiteDatabase db;
+                    db = helper.getWritableDatabase();
+                    helper.onCreate(db);
+                    helper.insert(question, answer);
+
+                }else{
+                    message.setChecked(false);
+
+                    String starTag = holder.starBtn.getTag().toString();
+                    String answerTag = holder.leftChatTextView.getTag().toString();
+                    String question =  holder.rightChatTextView.getText().toString().trim();
+                    String answer =  holder.leftChatTextView.getText().toString().trim();
+
+                    //지우기
+                    helper.Delete(question);
+
+                }
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return messageList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        LinearLayout leftChatView, rightChatView;
+        TextView leftChatTextView, rightChatTextView;
+        CheckBox starBtn;
+
+        public MyViewHolder(@NonNull View itemView){
+            super(itemView);
+            starBtn = itemView.findViewById(R.id.star_btn);
+            leftChatView = itemView.findViewById(R.id.left_chat_view);
+            rightChatView = itemView.findViewById(R.id.right_chat_view);
+            leftChatTextView = itemView.findViewById(R.id.left_chat_text_view);
+            rightChatTextView = itemView.findViewById(R.id.right_chat_text_view);
+
+        }
+    }
+}
